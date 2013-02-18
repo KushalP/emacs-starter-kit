cf9bf6a9327a961f78c9a0c54f51546aa2332ee4
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-18 10:11:10 +0000

    Add CSS and JS custom variables

--- custom-variables.el
+++ custom-variables.el
@@ -1,3 +1,7 @@
 (custom-set-variables
+ '(css-indent-offset 2)
+ '(js-indent-level 2)
+ '(js2-auto-indent-p nil)
+ '(js2-basic-offset 2)
  '(scala-interpreter "/usr/local/bin/scala")
  '(scss-compile-at-save nil))
